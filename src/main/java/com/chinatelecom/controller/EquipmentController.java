cd3290bd672ef432cb99929662af7ec9bf857a8b
Administrator <Administrator@SDWM-20151128QO>
2016-08-11 09:23:52 +0800

    项目提交

--- src/main/java/com/chinatelecom/controller/EquipmentController.java
+++ src/main/java/com/chinatelecom/controller/EquipmentController.java
@@ -1,5 +1,22 @@
 package com.chinatelecom.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.chinatelecom.dao.equipmentDao;
+
+@Controller
+@RequestMapping("/firststep")
 public class EquipmentController {
 
+    @Autowired
+    private equipmentDao ed;
+
+    @RequestMapping("/secondstep")
+    public Object test(@RequestParam(value = "nameid", required = false) String name) {
+        
+        return ed.getEquipInfoByEquipID(1);
+    }
 }
